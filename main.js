3fe8b82c23336770daaa0336be54a8b9cc8fe5e8
masterHeatwave <binaryJones0110@gmail.com>
2023-08-29 13:27:19 +0300

    Re-wrote the HTML using Bootstrap and applied styles for the banner and some other sections

---
+++ main.js
@@ -0,0 +1,47 @@
+function showSubMenu() {
+    const subMenu = document.getElementById('subMenu');
+    subMenu.style.display = 'block';
+}
+
+function hideSubMenu() {
+    const subMenu = document.getElementById('subMenu');
+    subMenu.style.display = 'none';
+}
+
+   function openPage(pageName, elmnt, color) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablink");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].style.backgroundColor = "";
+            }
+            document.getElementById(pageName).style.display = "block";
+            elmnt.style.backgroundColor = color;
+        }
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+
+        //accelarated css buttons
+        const tabs = document.querySelector(".wrapper");
+        const tabButton = document.querySelectorAll(".tab-button");
+        const contents = document.querySelectorAll(".content");
+
+        tabs.onclick = e => {
+            const id = e.target.dataset.id;
+            if (id) {
+                tabButton.forEach(btn => {
+                    btn.classList.remove("active");
+                });
+                e.target.classList.add("active");
+
+                contents.forEach(content => {
+                    content.classList.remove("active");
+                });
+                const element = document.getElementById(id);
+                element.classList.add("active");
+            }
+        }
